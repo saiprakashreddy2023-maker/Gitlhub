a738d60dfa3f5d124f9ce95a01051f33795d09e3
saiprakashreddy2023-maker <saiprakashreddy2023@gmail.com>
2026-01-21 21:57:57 +0530

    added name

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("welcome sai");
         System.out.println("how are you");
         System.out.println("name");
-        System.out.println("sai prakash reddy");
+        System.out.println("sai prakash reddy gopu");
 
 
 
