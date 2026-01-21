cb697157e557fbf2d5c721865c6d244ac840701d
saiprakashreddy2023-maker <saiprakashreddy2023@gmail.com>
2026-01-21 20:30:33 +0530

    comit chnages

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
+        System.out.println("how are you");
+        System.out.println("name");
 
 
         }
